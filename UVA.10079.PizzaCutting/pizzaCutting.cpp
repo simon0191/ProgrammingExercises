3d5c7329b0df44759153d9595a5f6828f13c9f21
Simon Soriano <simon0191@gmail.com>
2012-07-30 00:15:38 -0500

    new file:   UVA.10079.PizzaCutting/pizzaCutting 	new file:   UVA.10079.PizzaCutting/pizzaCutting.cpp 	new file:   commit.sh

---
+++ UVA.10079.PizzaCutting/pizzaCutting.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <cstring>
+#include <stack>
+#include <cctype>
+#include <limits>
+
+#define UL unsigned long long
+#define LL long long
+#define LD long double
+#define EPS 1e-9
+
+using namespace std;
+
+UL f(UL n){
+    return (n*(n+1)/2)+1;
+}
+int main(){
+    int n;
+	while(scanf("%d",&n) && n>=0){
+        printf("%llu\n",f(n));
+	}
+	return 0;
+}
