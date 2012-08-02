748b3a5d16f9d701acf8c9abb36780a8afd6a4ed
Simon Soriano <simon0191@gmail.com>
2012-08-01 22:59:34 -0500

    new file:   Circuito.2012.Central.B.ElevatorTrouble/elevatorTrouble.cpp 	new file:   UVA.855.LunchInGridCity/in 	new file:   UVA.855.LunchInGridCity/lunchInGridCity.cpp

---
+++ Circuito.2012.Central.B.ElevatorTrouble/elevatorTrouble.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <cstring>
+#include <cctype>
+#include <limits>
+
+#define UL unsigned long long
+#define LL long long
+#define LD long double
+#define EPS 1e-9
+
+using namespace std;
+
+void proc(vector<int>& vec,int act,int g,int u,int d,int size){
+    if(act == g)return;
+    int auxU = vec[act]+1;
+    if(act+u < size && vec[act+u] > auxU){
+        vec[act+u] = vec[act]+1;
+        proc(vec,act+u,g,u,d,size);
+    }
+    int auxD = vec[act]+1;
+    if(act-d >= 0 && vec[act-d] > auxD){
+        vec[act-d] = vec[act]+1;
+        proc(vec,act-d,g,u,d,size);
+    }
+
+}
+int main(){
+    int f,s,g,u,d;
+	while(scanf("%d%d%d%d%d",&f,&s,&g,&u,&d) && (f+s+g+u+d  )){
+        vector<int> vec(f,numeric_limits<int>::max());
+        vec[s-1] = 0;
+        proc(vec,s-1,g-1,u,d,f);
+        if(vec[g-1]!=numeric_limits<int>::max()){
+            printf("%d\n",vec[g-1]);
+        }
+        else{
+            puts("use the stairs");
+        }
+	}
+	return 0;
+}
+
---
+++ UVA.855.LunchInGridCity/lunchInGridCity.cpp
@@ -0,0 +1,49 @@
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <deque>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+
+#define UL unsigned long long
+#define LL long long
+#define LD long double
+#define EPS 1e-9
+
+const int AZ_SIZE = 'Z'-'A'+1;
+
+using namespace std;
+
+int main(){
+    int cases,r,c,n,med;
+    int s[50010];
+    int a[50010];
+	scanf("%d",&cases);
+	while(cases--){
+	    scanf("%d%d%d",&r,&c,&n);
+	    med = n/2;
+	    for(int i = 0;i<n;++i){
+	        scanf("%d%d",s+i,a+i);
+	    }
+	    if(n%2 == 0){
+            --med;
+	    }
+	    sort(a,a+n);
+	    sort(s,s+n);
+	    printf("(Street: %d, Avenue: %d)\n",s[med],a[med]);
+	}
+	return 0;
+}
+
+
