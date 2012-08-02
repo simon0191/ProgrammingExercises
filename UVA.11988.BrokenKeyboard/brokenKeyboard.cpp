87df5a25c6201e83deb5c79f01c16946b519b075
Simon Soriano <simon0191@gmail.com>
2012-08-01 20:57:45 -0500

    new file:   UVA.11988.BrokenKeyboard/BrokenKeyBoard.class 	new file:   UVA.11988.BrokenKeyboard/BrokenKeyBoard.java 	new file:   UVA.11988.BrokenKeyboard/BrokenKeyBoard.java~ 	new file:   UVA.11988.BrokenKeyboard/brokenKeyboard.cpp 	new file:   UVA.11988.BrokenKeyboard/brokenKeyboard.java 	new file:   UVA.11988.BrokenKeyboard/in

---
+++ UVA.11988.BrokenKeyboard/brokenKeyboard.cpp
@@ -0,0 +1,61 @@
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <deque>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+
+
+#define UL unsigned long long
+#define LL long long
+#define LD long double
+#define EPS 1e-9
+
+using namespace std;
+
+int main(){
+	char str[100010];
+	bool ini,end;
+
+	while(gets(str)){
+	    list<char> s;
+        list<char>::iterator it = s.begin();
+        bool ini = false;
+	    for(int i = 0;str[i];++i){
+	        switch(str[i]){
+	            case '[':
+                    it = s.begin();
+                    ini = true;
+	            break;
+	            case ']':
+                    ini = false;
+                    it = s.end();
+	            break;
+	            default:
+	            if(ini){
+                    s.insert(it,str[i]);
+	            }
+	            else{
+                    s.push_back(str[i]);
+	            }
+	        }
+	    }
+	    copy(s.begin(), s.end(),
+              std::ostream_iterator<char>(cout, ""));
+        cout<<endl;
+	}
+	return 0;
+}
+
+
---
+++ UVA.11988.BrokenKeyboard/brokenKeyboard.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+
+public class BrokenKeyBoard {
+	public static void main(String[] args) {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+}
