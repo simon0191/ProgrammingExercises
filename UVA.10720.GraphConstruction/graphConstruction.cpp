2c9454f9b41f68c231fa580c5c311306d4ef7e88
Simon Soriano <simon0191@gmail.com>
2012-08-08 10:53:08 -0500

    new file:   UVA.10720.GraphConstruction/graphConstruction.cpp 	new file:   UVA.10720.GraphConstruction/graphConstruction2.cpp 	new file:   UVA.10720.GraphConstruction/in 	new file:   UVA.10720.GraphConstruction/in2

---
+++ UVA.10720.GraphConstruction/graphConstruction.cpp
@@ -0,0 +1,72 @@
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <deque>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <vector>
+
+#define UL unsigned long long
+#define LL long long
+#define LD long double
+#define EPS 1e-9
+
+const int AZ_SIZE = 'Z'-'A'+1;
+
+using namespace std;
+
+int main(){
+    int n,sum,sum2,noNull;
+    bool first = true;
+    vector<int> ds;
+    bool flag;
+	while(scanf("%d",&n)==1 && n){
+	    ds.assign(n+1,0);
+	    noNull = sum = 0;
+	    flag = true;
+	    for(int i = 1;i<=n;++i){
+	        scanf("%d",&ds[i]);
+	        if(ds[i]<0)flag = false;
+	        sum+=ds[i];
+	        if(ds[i])noNull++;
+	    }
+	    sort(ds.begin(),ds.end());
+	    if(n==1 && ds[1]>0)flag = false;
+        if(flag){
+            if((sum % 2)||(sum > noNull*(noNull - 1))) flag = false;
+            else{
+                sum = 0;
+                flag = true;
+                for(int i = 1;i<=n;++i){
+                    sum+=ds[i];
+                    sum2 = i*(i-1);
+                    for(int j = i+1;j<=n;++j){
+                        sum2+=min(i,ds[j]);
+                    }
+                    if(sum > sum2){
+                        flag = false;
+                        break;
+                    }
+                }
+            }
+        }
+	    /*if(!first)puts("");
+	    else first = false;*/
+	    printf("%s\n",( flag?"Possible":"Not possible" ));
+	}
+	return 0;
+}
+
+
+
---
+++ UVA.10720.GraphConstruction/graphConstruction2.cpp
@@ -0,0 +1,77 @@
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <deque>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <vector>
+
+#define UL unsigned long long
+#define LL long long
+#define LD long double
+#define EPS 1e-9
+
+const int AZ_SIZE = 'Z'-'A'+1;
+
+using namespace std;
+
+bool f(priority_queue<int>& q){
+    if(q.empty())return false;
+    do{
+        int act = q.top();
+        q.pop();
+        if(q.size() >= act){
+            vector<int> v;
+            for(int i = 0;i<act;++i){
+                if(q.top() > 1)v.push_back(q.top()-1);
+                q.pop();
+            }
+            for(int i = 0;i<v.size();++i){
+                q.push(v[i]);
+            }
+        }
+        else{
+            return false;
+        }
+    }while(!q.empty());
+    return true;
+}
+
+int main(){
+	int n,sum,sum2,noNull;
+    int aux;
+    bool possible;
+	while(scanf("%d",&n)==1 && n){
+	    noNull = sum = 0;
+	    possible = true;
+	    bool allZero = true;
+	    priority_queue<int> q;
+	    for(int i = 0;i<n;++i){
+	        scanf("%d",&aux);
+	        if(aux<0)possible = false;
+	        if(aux) allZero = false;
+	        if(aux>0)q.push(aux);
+	    }
+        if(allZero || (possible && (possible = f(q)))){
+            puts("Possible");
+        }
+        else{
+            puts("Not possible");
+        }
+	}
+	return 0;
+}
+
+
+
