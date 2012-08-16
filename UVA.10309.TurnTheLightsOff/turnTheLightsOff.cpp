02264293b3d42af4abdf9f0203af1d7d2e2c18dc
Simon Soriano <simon0191@gmail.com>
2012-08-15 19:44:09 -0500

    new file:   UVA.10309.TurnTheLightsOff/in 	new file:   UVA.10309.TurnTheLightsOff/in2 	new file:   UVA.10309.TurnTheLightsOff/turnTheLightsOff.cpp 	new file:   UVA.11157.DynamicFrog/dynamicFrog.cpp 	new file:   UVA.11157.DynamicFrog/in 	new file:   UVA.11157.DynamicFrog/in2

---
+++ UVA.10309.TurnTheLightsOff/turnTheLightsOff.cpp
@@ -0,0 +1,88 @@
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <deque>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <vector>
+
+#define UL unsigned long long
+#define LL long long
+#define LD long double
+#define EPS 1e-9
+
+const int AZ_SIZE = 'Z'-'A'+1;
+
+using namespace std;
+
+void _switch(vector<int>& mat,int x, int y){
+    mat[x]^= (1<<y);
+    if(x-1 >=0)mat[x-1]^=(1<<y);
+    if(y-1 >=0)mat[x]^=(1<<(y-1));
+    if(x+1 <10)mat[x+1]^=(1<<(y));
+    if(y+1 <10)mat[x]^=(1<<(y+1));
+}
+bool verify(vector<int>& mat){
+    for(int i = 0;i<10;++i){
+        if(mat[i])return false;
+    }
+    return true;
+}
+bool f(vector<int> mat,int mask,int &cont){
+    cont = 0;
+    for(int i = 0;i<10;++i){
+        if(mask&(1<<i)){
+            _switch(mat,0,i);
+            ++cont;
+        }
+    }
+    for(int i = 1;i<10;++i){
+        for(int j = 0;j<10;++j){
+            if( mat[i-1]&(1<<j) ){
+                _switch(mat,i,j);
+                ++cont;
+            }
+        }
+    }
+    return verify(mat);
+}
+int main(){
+	char name[1024],buff[16];
+	int cont;
+	vector<int> mat;
+	while(scanf("%s",name)==1 && strcmp(name,"end")){
+	    mat.assign(10,0);
+        for(int i = 0;i<10;++i){
+            scanf("%s",buff);
+            for(int j = 0;buff[j];++j){
+                if(buff[j]=='O')mat[i]|=(1<<j);
+            }
+        }
+        int len = (1<<10);
+        int minn = 100;
+        int cont;
+        for(int i = 0;i<len;++i){
+            cont = 0;
+            if(f(mat,i,cont)){
+                minn = min(minn,cont);
+            }
+        }
+        if(minn > 100)minn = -1;
+        printf("%s %d\n",name,minn);
+	}
+	return 0;
+}
+
+
+
---
+++ UVA.11157.DynamicFrog/dynamicFrog.cpp
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <deque>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <vector>
+
+#define UL unsigned long long
+#define LL long long
+#define LD long double
+#define EPS 1e-9
+
+const int AZ_SIZE = 'Z'-'A'+1;
+
+using namespace std;
+
+int main(){
+	int cases,n,d,dis;
+	char t[5];
+	int dist[1000];
+	scanf("%d",&cases);
+	for(int c = 1;c<=cases;++c){
+	    scanf("%d %d",&n,&d);
+	    int size = 0;
+	    dist[size++] = 0;
+
+	    for(int i = 1;i<=n;++i){
+	        scanf("%s",t);
+	        dis = atoi(t+2);
+            dist[size++] = dis;
+	        if(t[0]=='B'){
+	            dist[size++] = dis;
+	        }
+	    }
+	    dist[size++] = d;
+	    int maxx = 0;
+	    for(int i = 2;i<size;++i){
+            maxx = max(maxx,dist[i]-dist[i-2]);
+	    }
+	    printf("Case %d: %d\n",c,maxx);
+	}
+	return 0;
+}
+
+
+
